7b9dad7f3c7bf3708ce253cb684851381d630cee
ethicalanp <ethicalanp66@gmail.com>
2025-12-18 10:48:47 +0530

    urls.py

--- todoList/todoApp/urls.py
+++ todoList/todoApp/urls.py
@@ -5,6 +5,5 @@ urlpatterns=[
     path('',todo_list,name='todo_list'),
     path('complete/<int:todo_id>/',complete,name='complete'),
     path('delete/<int:todo_id>/',delete,name='delete'),
-    path('edit/<int:todo_id>/',edit,name='edit'),
-    
+    path('edit/<int:todo_id>/',edit,name='edit'),  
 ]
